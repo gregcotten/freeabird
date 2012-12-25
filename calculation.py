b5a39dbd2217c5677a7b30dcd60982cfbef0a032
Greg Cotten <gregcotten@gmail.com>
2012-12-25 11:36:19 -0600

    Revert "Added imap function"
    This reverts commit 8aaa1a6c2ddaf09992baba09e4cdbb2b7fd9eb9b.

--- calculation.py
+++ calculation.py
@@ -16,7 +16,6 @@ def product_to_list(product):
 	for i in product:
 		list_products.append(i)
 	return list_products
-<<<<<<< HEAD
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 os.chdir("Data")
 
@@ -55,34 +54,3 @@ for combination in all_combinations:
 		less_than_fivehundredcalories.append(combination)
 
 print "All Combinations: " + str(len(all_combinations)) + "\n500 Calories or less: " + str(len(less_than_fivehundredcalories))	
-=======
-	
-def imap(function, *iterables):
-	# imap(pow, (2,3,10), (5,2,3)) --> 32 9 1000
-	iterables = map(iter, iterables)
-	while True:
-		args = [next(it) for it in iterables]
-		if function is None:
-			yield tuple(args)
-		else:
-			yield function(*args)
-	
-#exclusive means we can only choose one from the list
-list1_exclusive = [1,2,8]
-list2_exclusive = [3,4,7]
-list3_exclusive = [12,6,5]
-
-#inclusive means it can be anything from the list - any amount, order doesn't matter
-list4_inclusive = [8, 10, 20, 30, 50]
-
-product_of_exclusives = product_to_list(itertools.product(list1_exclusive, list2_exclusive, list3_exclusive))
-
-list4_all_combinations = all_combinations(list4_inclusive)
-
-
-
-for i in product_to_list(itertools.product(product_of_exclusives, list4_all_combinations)):
-	print i
-	print imap(pow, (2,3,10), (5,2,3)) #Why doesn't this result in "32 9 1000"?
-	
->>>>>>> Added imap function
