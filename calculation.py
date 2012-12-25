03a1e5051c8f81066b2aa02a4ad5d086bac36b9c
Greg Cotten <gregcotten@gmail.com>
2012-12-25 02:53:33 -0600

    ok great it works for freebird burritos
    only works with the trimmed down CSV for the regular freebird burrito

--- calculation.py
+++ calculation.py
@@ -1,10 +1,14 @@
-import itertools
+import itertools, csv, os, copy
 
 def all_combinations(list):
 	list_all_combinations = []
 	for i in range(1, len(list)):
 		for j in itertools.combinations(list, i):
-			list_all_combinations.append(j)
+			tuple = []
+			for k in j:
+				if k is not None:
+					tuple.append(k)
+			list_all_combinations.append(tuple)
 	return list_all_combinations
 
 def product_to_list(product):
@@ -12,22 +16,41 @@ def product_to_list(product):
 	for i in product:
 		list_products.append(i)
 	return list_products
-	
-#exclusive means we can only choose one from the list
-list1_exclusive = [1,2,8]
-list2_exclusive = [3,4,7]
-list3_exclusive = [12,6,5]
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir("Data")
 
-#inclusive means it can be anything from the list - any amount, order doesn't matter
-list4_inclusive = [8, 10, 20, 30, 50]
+list = []
+with open("freebirds_caloric_info_freebird_burrito.csv") as data:
+	reader = csv.reader(data, delimiter=",")
+	for row in reader:
+		list.append(row)
 
-product_of_exclusives = product_to_list(itertools.product(list1_exclusive, list2_exclusive, list3_exclusive))
+exclusive_list = [] #right now just different tortillas should be exclusive
+inclusive_list = [] #everything else can supposedly be mashed together in a burrito
+for i in range(1,5): #hardcode first four options (tortillas) are the exclusive options
+	exclusive_list.append(list[i])
+for i in range(5, len(list)): #hardcode rest of options as inclusive anything goes
+	inclusive_list.append(list[i])
 
-list4_all_combinations = all_combinations(list4_inclusive)
+inclusive_all_combinations = all_combinations(inclusive_list)
 
+#get all possible combinations of burritos
+all_combinations = []
+for i in exclusive_list:
+	for j in inclusive_all_combinations:
+		lst = []
+		for k in j:
+			lst.append(k)
+		lst.insert(0,i)
+		all_combinations.append(lst)
 
+less_than_fivehundredcalories = []
 
-for i in product_to_list(itertools.product(product_of_exclusives, list4_all_combinations)):
-	print i
-	
-	
+for combination in all_combinations:
+	calorie_sum = 0
+	for item in combination:
+		calorie_sum += int(item[2])
+	if calorie_sum <= 500:
+		less_than_fivehundredcalories.append(combination)
+
+print "All Combinations: " + str(len(all_combinations)) + "\n500 Calories or less: " + str(len(less_than_fivehundredcalories))	
