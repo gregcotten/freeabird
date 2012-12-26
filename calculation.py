a73af8f3de1e125af4c5b10c5d820287993092f5
Greg Cotten <gregcotten@gmail.com>
2012-12-25 22:23:30 -0600

    fixed an issue with calculating combinations

--- calculation.py
+++ calculation.py
@@ -9,7 +9,7 @@ def calorie_count(combination):
 
 def all_combinations_from_list(list):
 	list_all_combinations = []
-	for i in range(1, len(list)):
+	for i in range(1, len(list)+1):
 		for j in itertools.combinations(list, i):
 			tuple = []
 			for k in j:
@@ -20,7 +20,7 @@ def all_combinations_from_list(list):
 	
 def all_combinations_with_cal_less_than_or_equal_to(list,cal_count):
 	combinations_less_than = []
-	for i in range(1, len(list)):
+	for i in range(1, len(list)+1):
 		for j in itertools.combinations(list, i):
 			tuple = []
 			for k in j:
@@ -52,13 +52,13 @@ def do_optimized_calculation(filename,max_cal):
 
 	exclusive_list = [] #right now just different tortillas should be exclusive
 	inclusive_list = [] #everything else can supposedly be mashed together in a burrito
-	for i in range(1,4): #hardcode exclusive options
+	
+	for i in range(1,5): #hardcode exclusive options
 		exclusive_list.append(data_list[i])
 	for i in range(5, len(data_list)): #hardcode rest of options as inclusive anything goes
 		inclusive_list.append(data_list[i])
 
 	inclusive_all_combinations = all_combinations_from_list(inclusive_list)
-
 
 	all_combinations = []
 	less_than_maxcal_combinations = []
